a7b447bf6f2b10efee52c283d64127cef1ad914b
Dhileepguru <dhileepguru497@gmail.com>
2024-01-25 10:08:17 +0530

    included scrolly

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,14 @@
   AOS.init();
 </script>
 
+window.addEventListener("scroll",function(){
+  var header=document.querySelector(".fixedheader");
+  var scrollPosition=window.scrollY;
+
+  if(scrollPosition>0){
+    header.classList.add("scrolled");
+  }
+  else{
+    header.classList.remove("scrolled");
+  }
+})
